bdb5847f6f24de53cca224524e84dc4849eebc07
neo5906 <162334739+neo5906@users.noreply.github.com>
2025-03-17 18:54:14 +0800

    Add files via upload

---
+++ assignment4/assignment4_program1/assignment4_program1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4_program1
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T data)
+        {
+            Next = null;
+            Data = data;
+        }
+    }
+
+    // 泛型链表类  
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+ 
+        public void ForEach(Action<T> action)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                action(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {  
+            GenericList<int> list = new GenericList<int>();
+            string s = Console.ReadLine();
+            int[] arr = s.Split(' ').Select(int.Parse).ToArray();
+            foreach (int d in arr)
+            {
+                list.Add(d);
+            }
+
+            Console.WriteLine("列表:");
+            list.ForEach(item => Console.WriteLine(item));
+  
+            int max = int.MinValue;
+            list.ForEach(item =>
+            {
+                if (item > max)
+                    max = item;
+            });
+            Console.WriteLine($"最大值: {max}");
+  
+            int min = int.MaxValue;
+            list.ForEach(item =>
+            {
+                if (item < min)
+                    min = item;
+            });
+            Console.WriteLine($"最小值: {min}");
+
+            int sum = 0;
+            list.ForEach(item => sum += item);
+            Console.WriteLine($"总和: {sum}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignment4/assignment4_program2/assignment4_program2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace assignment4_program2
+{
+    public class AlarmClock
+    {
+        private Timer timer;
+        private DateTime alarmTime;
+        private bool isAlarmSet;
+
+        public event Action Tick;
+        public event Action Alarm;
+
+        public AlarmClock()
+        {
+            timer = new Timer(1000); // 每秒触发一次  
+            timer.Elapsed += OnTick;
+        }
+
+        public void Start()
+        {
+            timer.Start();
+            Console.WriteLine("闹钟启动");
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+            Console.WriteLine("闹钟停止");
+        }
+
+        public void SetAlarm(DateTime time)
+        {
+            alarmTime = time;
+            isAlarmSet = true;
+            Console.WriteLine($"闹钟设置为 {alarmTime.ToString("HH:mm:ss")}");
+        }
+
+        private void OnTick(object sender, ElapsedEventArgs e)
+        {
+            Tick?.Invoke(); // 触发 Tick 事件  
+            Console.WriteLine("嘀嗒...");
+
+            if (isAlarmSet && DateTime.Now >= alarmTime)
+            {
+                Alarm?.Invoke(); 
+                isAlarmSet = false; 
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlarmClock alarmClock = new AlarmClock();          
+            alarmClock.Alarm += () => { Console.WriteLine("闹钟响铃！时间到！"); };
+            alarmClock.Start();
+            alarmClock.SetAlarm(DateTime.Now.AddSeconds(5));
+            Console.WriteLine("按任意键停止闹钟");
+            Console.ReadKey();
+            alarmClock.Stop();
+        }
+    }
+}
